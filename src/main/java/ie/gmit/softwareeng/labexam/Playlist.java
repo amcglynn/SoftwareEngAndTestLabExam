8b774ad1a2b0ae0da8c9f8fb051f6db19e958434
amcglynn <amcglynn@cisco.com>
2019-04-04 22:29:48 +0100

    Added Playlist class so the code would compile

---
+++ src/main/java/ie/gmit/softwareeng/labexam/Playlist.java
@@ -0,0 +1,71 @@
+package ie.gmit.softwareeng.labexam;
+
+public class Playlist {
+    public Playlist(String playlistName) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public String getName() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public boolean contains(Song song) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public void addSong(Song song) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public void removeSong(Song song) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public int getNumberOfSongs() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public Song getSongAt(int index) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public void clear() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public void reverse() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public Song[] getAllSongs() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public void shuffle() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public Song getCurrentSong() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public Song getNextSong() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public void nextSong() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public Song getPreviousSong() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public void previousSong() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public void goToIndex(int index) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+}
