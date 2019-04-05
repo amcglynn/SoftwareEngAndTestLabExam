df8864de2d7144bdd1e9546e54f72f71979138ec
EOGHAN LAWLESS - Student <G00324903@gmit.ie>
2019-04-05 14:40:18 +0100

    Implemented goToIndex

--- src/main/java/ie/gmit/softwareeng/labexam/Playlist.java
+++ src/main/java/ie/gmit/softwareeng/labexam/Playlist.java
@@ -100,6 +100,6 @@ public class Playlist {
     }
 
     public void goToIndex(int index) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        currentSong = songs.get(index);
     }
 }
